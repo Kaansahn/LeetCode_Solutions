5fd3b8fa73ca546c4e4eba3775b06184569e1b77
Kaan Sahin <kaan.sahin2@hotmail.com>
2025-01-31 13:59:47 +0300

    Time: 0 ms (100%), Space: 48.3 MB (58.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,13 +1,16 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> seen = new Dictionary<int, int>();
-
-        for(int i = 0; i < nums.Length; i++){
-            if(seen.ContainsKey(target - nums[i])){
-                return new int[] {seen[target-nums[i]], i};
-            }
-            if(!seen.ContainsKey(nums[i])){
-                seen.Add(nums[i], i);
+        for(int i = 0; i < nums.Length - 1; i++)
+        {
+            for(int j = i+1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[] {i, j};
+                }else if(nums[j] + nums[j - 1] == target)
+                {
+                    return new int[] {j, j - 1};
+                }
             }
         }
         return null;
