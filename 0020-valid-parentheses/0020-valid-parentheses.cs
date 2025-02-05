74e143043484938687505f6174b1b1bbe289dcb9
Kaan Sahin <kaan.sahin2@hotmail.com>
2025-02-05 16:45:24 +0300

    Time: 2 ms (91.14%), Space: 41.7 MB (52.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new();
+
+        foreach(char c in s)
+        {
+            if(c == '(')
+            {
+                stack.Push(')');
+                continue;
+            }
+            if(c == '['){
+                stack.Push(']');
+                continue;
+            }
+            if(c == '{')
+            {
+                stack.Push('}');
+                continue;
+            }
+            if(stack.Count == 0 || c != stack.Pop()) return false;
+        }
+
+        return stack.Count == 0;
+    }
+}
