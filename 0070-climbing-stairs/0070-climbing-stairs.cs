ad105cad0040a94ee2f9b2f525ea8f7d272fe11d
Kaan Sahin <kaan.sahin2@hotmail.com>
2025-02-06 17:56:02 +0300

    Time: 0 ms (100%), Space: 29.5 MB (5.86%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int prev1 = 1;
+        int prev2 = 2;
+        int result = 0;
+
+        for(int i = 3; i <= n; i++)
+        {
+            result = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = result;
+        }
+
+        return result; 
+    }
+}
