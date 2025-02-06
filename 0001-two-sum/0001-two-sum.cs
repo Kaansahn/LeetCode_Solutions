0c5b463dfa7082758da46beb8328dae1c1a63a4c
Kaan Sahin <kaan.sahin2@hotmail.com>
2025-02-06 17:42:13 +0300

    Time: 0 ms (100%), Space: 47.8 MB (87.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -5,6 +5,7 @@ public class Solution {
                 for(int j = i+1; j < arr.Length; j++)
                 {
                     if(arr[i] + arr[j] == target) return new int[] {i, j};
+                    else if(arr[j] + arr[j - 1] == target) return new int[] {j, j-1};
                 }
             }
             return null;
